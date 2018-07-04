fc84a952a5242834c0ab7c918fd0f24af95828b7
Dami <obayanjud@gmail.com>
2018-07-04 13:52:44 -0600

    write function to make the grid

--- designs.js
+++ designs.js
@@ -5,10 +5,17 @@ const color = colorInput.value;
 // Select size input
 const width = document.querySelector("#inputWidth").value;
 const height = document.querySelector("#inputHeight").value;
-// console.log();
 
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
-  // Your code goes here!
+  const grid = document.querySelector("table");
+  // console.log(grid);
+  for (const y = 0; y < height; y++) {
+    tableRow = document.createElement("tr");
+    grid.appendChild(tableRow);
+    for (const x = 0; x < width; x++) {
+      tableColumn = document.createElement("td");
+      grid.appendChild(tableColumn);
+    }
+  }
 }
