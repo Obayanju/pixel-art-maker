07d341980daceb0d64935d2fec3a76fe62207822
Dami <obayanjud@gmail.com>
2018-07-03 18:14:21 -0600

    rename design.js

---
+++ designs.js
@@ -0,0 +1,15 @@
+
+
+// Select color input
+// const colorInput = document.querySelector("input");
+// console.log(colorInput.value);
+// colorInput.addEventListener("input", function() {
+//     console.log("color has been changed");
+// });
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+    // Your code goes here!
+  }
