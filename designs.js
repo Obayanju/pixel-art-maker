6f89299fa40885f4a6cfb0a46ff9de073d603be4
Dami <obayanjud@gmail.com>
2018-07-04 14:56:07 -0600

    give table row and table column types, and adjust their scope

--- designs.js
+++ designs.js
@@ -9,12 +9,12 @@ const height = document.querySelector("#inputHeight").value;
 // When size is submitted by the user, call makeGrid()
 function makeGrid() {
   const grid = document.querySelector("table");
+  const tableRow = document.createElement("tr");
+  const tableColumn = document.createElement("td");
   // console.log(grid);
   for (let y = 0; y < height; y++) {
-    tableRow = document.createElement("tr");
     grid.appendChild(tableRow);
     for (let x = 0; x < width; x++) {
-      tableColumn = document.createElement("td");
       grid.appendChild(tableColumn);
     }
   }
