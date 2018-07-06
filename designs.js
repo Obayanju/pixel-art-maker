ebd8090c4d731066ef480b3c8e23882780a6f996
Dami <obayanjud@gmail.com>
2018-07-05 23:50:16 -0600

    fix for loop looping only once

--- designs.js
+++ designs.js
@@ -6,14 +6,19 @@ const color = colorInput.value;
 function getSize() {
   const height = document.querySelector("#inputHeight").value;
   const width = document.querySelector("#inputWidth").value;
+  return [width, height];
 }
 
 // When size is submitted by the user, call makeGrid()
 function makeGrid() {
+  // get the users size input
+  const size = getSize();
+  const width = size[0];
+  const height = size[1];
   const grid = document.querySelector("table");
   const tableRow = document.createElement("tr");
   const tableColumn = document.createElement("td");
-  // console.log(grid);
+  console.log(width, height);
   for (let y = 0; y < height; y++) {
     grid.appendChild(tableRow);
     for (let x = 0; x < width; x++) {
